137c07079a090d7e1d6ba06c9c6eae85b01ad659
Katze719 <katze719@users.noreply.github.com>
2024-09-05 17:16:27 +0000

    fix: :bug: correct title

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>www.test</title>
+        <title>AgD - Aktion gegen Drucker</title>
         <link rel="stylesheet" href="/styles.css" />
         <link rel="stylesheet" href="/bootstrap/css/bootstrap.css" />
         <script src="/bootstrap/js/bootstrap.bundle.js"></script>
