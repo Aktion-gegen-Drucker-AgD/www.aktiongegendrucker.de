2fe37702f909eb5d056001e07ddd267544438dd1
Katze719 <katze719@users.noreply.github.com>
2024-09-05 18:08:58 +0000

    feat: :sparkles: use port 80

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,3 +1,7 @@
 import { defineConfig } from "$fresh/server.ts";
 
-export default defineConfig({});
+const port = Number(Deno.env.get("PORT")) || 8000;
+
+export default defineConfig({
+    port: port,
+});
