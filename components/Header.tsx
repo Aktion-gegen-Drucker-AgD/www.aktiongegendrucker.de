839e26c4bc424c7480023cc6d600a6bb523e0c03
Katze719 <katze719@users.noreply.github.com>
2024-09-06 16:49:56 +0000

    feat: :sparkles: add bootstrap

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,29 +6,34 @@ export function Header() {
                     <img src="/images/agd-logo.png" alt="Logo" />
                 </a>
 
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="#home">Home</a>
-                        </li>
-                        <li>
-                            <a href="#services">Services</a>
-                        </li>
-                        <li>
-                            <a href="#about">Über uns</a>
-                        </li>
-                        <li>
-                            <a href="#contact">Kontakt</a>
-                        </li>
-                        <li>
-                            <a href="#contact">Kontakt</a>
-                        </li>
-                        {/* <li>
-                            <a href="#">
-                                <img src="search-icon.png" alt="Search" />
-                            </a>
-                        </li> */}
-                    </ul>
+                <nav class="navbar navbar-expand-lg">
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#home">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#services">Services</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#about">Über uns</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#contact">Kontakt</a>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
             </div>
         </header>
