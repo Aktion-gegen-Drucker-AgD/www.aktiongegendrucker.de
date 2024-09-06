d78cef39b15c1fa18a5904ed809d86e1ccc40d0b
TechN1CKer <github@nick-vollmer.de>
2024-09-06 17:45:12 +0000

    feat: :art: added the "kontakt" page

---
+++ routes/kontakt.tsx
@@ -0,0 +1,21 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Kontakt() {
+    return (
+        <>
+            <Head>
+                <title>Kontakt</title>
+            </Head>
+            <div>
+                <div style="height:100vh">
+                    <img
+                        src="/images/putin-on-a-bear.jpg"
+                        alt="Berliiiiin"
+                        style="width: 100%;object-fit: cover;object-position: center;display: block;"
+                    >
+                    </img>
+                </div>
+            </div>
+        </>
+    );
+}
