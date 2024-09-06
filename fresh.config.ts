fb6f9a6e4930912938e3d6716802e3de41ae952b
Katze719 <katze719@users.noreply.github.com>
2024-09-06 16:49:56 +0000

    feat: :sparkles: ssl first try

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "$fresh/server.ts";
 
-const port = Number(Deno.env.get("PORT")) || 8000;
+const port = Number(Deno.env.get("DENO_PORT")) || 8000;
+
+const certFile = Deno.env.get("DENO_CERTFILE") || undefined;
+const keyFile = Deno.env.get("DENO_KEYFILE") || undefined;
 
 export default defineConfig({
     port: port,
+    cert: certFile,
+    key: keyFile,
 });
