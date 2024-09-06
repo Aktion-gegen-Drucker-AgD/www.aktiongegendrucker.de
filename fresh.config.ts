47c123ab72ac0febebfcdf940c55dc1bb5399f1c
Katze719 <katze719@users.noreply.github.com>
2024-09-06 16:49:56 +0000

    fix: :bug: use cert as text file

--- fresh.config.ts
+++ fresh.config.ts
@@ -5,21 +5,11 @@ const port = Number(Deno.env.get("DENO_PORT")) || 8000;
 const certFile = Deno.env.get("DENO_CERTFILE");
 const keyFile = Deno.env.get("DENO_KEYFILE");
 
-if (certFile && keyFile) {
-    try {
-        await Deno.stat(certFile);
-        await Deno.stat(keyFile);
-    } catch (error) {
-        console.error("Error accessing certificate or key file:", error);
-        Deno.exit(1);
-    }
-} else {
-    console.error("Cert file or key file not specified.");
-    Deno.exit(1);
-}
+const cert = certFile ? await Deno.readTextFile(certFile) : undefined;
+const key = keyFile ? await Deno.readTextFile(keyFile) : undefined;
 
 export default defineConfig({
     port: port,
-    cert: certFile,
-    key: keyFile,
+    cert: cert,
+    key: key,
 });
