4934ca3dfe02f270459040ed771b17f410cc0a00
Katze719 <katze719@users.noreply.github.com>
2024-09-06 16:49:56 +0000

    fix: :bug: correct title

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
     return (
         <footer class="footer">
             <div class="container">
-                <p class="text-center">© 2024 Dein Unternehmen</p>
+                <p class="text-center">© 2024 AgD - Aktion gegen Drucker</p>
             </div>
         </footer>
     );
