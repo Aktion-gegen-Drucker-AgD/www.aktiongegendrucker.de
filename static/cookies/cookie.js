d3ffd67e9783326358ff9895dbd5724bb8e1976b
TechN1CKer <github@nick-vollmer.de>
2024-09-06 16:49:56 +0000

    feat: :art: added the cookie banner that will always reapear

---
+++ static/cookies/cookie.js
@@ -0,0 +1,5 @@
+// Cookie Banner functionality
+document.getElementById('cookie-btn').addEventListener('click', function() {
+    let banner = document.getElementById('cookie-banner');
+    banner.style.display = 'none';
+});
