26e27791aaa994b7fca66b1b6eca818750079c28
Katze719 <katze719@users.noreply.github.com>
2024-09-06 16:49:56 +0000

    feat: :sparkles: adjust a few thing on the index

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,36 +1,40 @@
-
-
 export default function Home() {
   return (
     <main>
-        <section class="hero">
-            <img src="/images/index_bg_top.png" alt="Landscape Image" />
-            <div class="newsletter">
-                <form>
-                    <input type="email" placeholder="E-Mail Adresse eingeben" required />
-                    <label>
-                        <input type="checkbox" required /> Die <a href="#">Datenschutzhinweise</a> habe ich gelesen und stimme zu
-                    </label>
-                    <button type="submit">AfD-Infobrief Abonnieren</button>
-                </form>
-            </div>
-        </section>
+      <section class="hero">
+        <img src="/images/index_bg_top.png" alt="Landscape Image" />
+        <div class="newsletter">
+          <form>
+            <input
+              type="email"
+              placeholder="E-Mail Adresse eingeben"
+              required
+            />
+            <label>
+              <input type="checkbox" required /> Die{" "}
+              <a href="#">Datenschutzhinweise</a> habe ich gelesen und stimme zu
+            </label>
+            <button type="submit">AgD-Infobrief Abonnieren</button>
+          </form>
+        </div>
+      </section>
 
-        <section class="actions">
-            <button>Mitglied werden!</button>
-            <button>Jetzt spenden!</button>
-            <button>Infobrief erhalten!</button>
-        </section>
+      <div class="button-row">
+        <button>Button 1</button>
+        <button>Button 2</button>
+        <button>Button 3</button>
+      </div>
 
-        <section class="leaders">
-            <div class="leader">
-                <img src="/images/leader_right.png" alt="Alice Weidel" />
-                <p>Alice Weidel / Tino Chrupalla: Das Verbot des...</p>
-            </div>
-            <div class="leader">
-                <img src="/images/leader_right.png" alt="Other Leader" />
-                <p>Another leader's content...</p>
-            </div>
-        </section>
-    </main>  );
+      <section class="leaders">
+        <div class="leader">
+          <img src="/images/leader_right.png" alt="Alice Weidel" />
+          <p>Alice Weidel / Tino Chrupalla: Das Verbot des...</p>
+        </div>
+        <div class="leader">
+          <img src="/images/leader_right.png" alt="Other Leader" />
+          <p>Another leader's content...</p>
+        </div>
+      </section>
+    </main>
+  );
 }
