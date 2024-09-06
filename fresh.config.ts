317e5957fd401ae8435a6458499804e2c0a68a0c
Katze719 <katze719@users.noreply.github.com>
2024-09-06 16:49:56 +0000

    fix: :bug: debug step 1

--- fresh.config.ts
+++ fresh.config.ts
@@ -2,8 +2,21 @@ import { defineConfig } from "$fresh/server.ts";
 
 const port = Number(Deno.env.get("DENO_PORT")) || 8000;
 
-const certFile = Deno.env.get("DENO_CERTFILE") || undefined;
-const keyFile = Deno.env.get("DENO_KEYFILE") || undefined;
+const certFile = Deno.env.get("DENO_CERTFILE");
+const keyFile = Deno.env.get("DENO_KEYFILE");
+
+if (certFile && keyFile) {
+    try {
+        await Deno.stat(certFile);
+        await Deno.stat(keyFile);
+    } catch (error) {
+        console.error("Error accessing certificate or key file:", error);
+        Deno.exit(1);
+    }
+} else {
+    console.error("Cert file or key file not specified.");
+    Deno.exit(1);
+}
 
 export default defineConfig({
     port: port,
