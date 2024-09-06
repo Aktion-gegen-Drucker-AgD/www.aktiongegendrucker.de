97a7dadf9a2898e7282b5680ce1a70adfea2d8a3
Katze719 <katze719@users.noreply.github.com>
2024-09-06 16:49:56 +0000

    feat: :sparkles: added header + part of index

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+export function Header() {
+    return (
+        <header>
+            <div class="main">
+                <a href="#home" class="logo">
+                    <img src="/images/agd-logo.png" alt="Logo" />
+                </a>
+
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#home">Home</a>
+                        </li>
+                        <li>
+                            <a href="#services">Services</a>
+                        </li>
+                        <li>
+                            <a href="#about">Über uns</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Kontakt</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Kontakt</a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="search-icon.png" alt="Search" />
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
